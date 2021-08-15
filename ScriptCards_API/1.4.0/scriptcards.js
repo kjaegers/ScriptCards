e100ca1039bd5e3471eb4eebce6771802ad1377a
John Sherwood <jsherwood0@rootwheel.org>
2021-08-15 00:31:07 -0400

    Fix broken counting of paren-wrapped numbers

--- ScriptCards_API/1.4.0/scriptcards.js
+++ ScriptCards_API/1.4.0/scriptcards.js
@@ -2769,6 +2769,10 @@ const ScriptCards = (() => { // eslint-disable-line no-unused-vars
 				rollResult.Text += currentOperator == "*" ? "x " : currentOperator + " ";
 			}
 
+			// A bare number within parens (just strip them)
+			if (text.match(/^\([+-]?(\d*\.)?\d*\)$/)) {
+				text = text.substring(1, text.length - 1)
+			}
 			// Just a number
 			if (text.match(/^[+-]?(\d*\.)?\d*$/)) {
 				componentHandled = true;
