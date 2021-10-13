95ce9138608a9e3ab74d283d2ce1ae31f8d55ca5
Kurt Jaegers <kurt.jaegers@live.com>
2021-10-13 18:26:05 -0400

    Create X_OtherScripts and add APIAlerter

---
+++ X_OtherScripts/APIAlerter.js
@@ -0,0 +1,11 @@
+on("ready", function() {
+    var d = new Date();
+    var html = "<table border=2 width=100%><tr><td bgcolor=goldenrod colspan=2 align=center><font color=black><strong>API Sandbox Alert</strong></font></td></tr>";
+    html += `<tr><td colspan=2>The API Sandbox has started or restarted (${d.toDateString()} ${d.toTimeString()}). If this is unexpected, it is likely that your sandbox crashed. `;
+    html += "If that is the case, it is recommended that you manually restart the sandbox, as API scripts running in your game are/will become ";
+    html += "unstable.<br /><br /><strong>NOTE:</strong> This is expected behavior if you just launched your game or made updates to your running API ";
+    html += "scripts.";
+    html += "</td></tr></table>";
+
+    sendChat("PCMHelper:", html);
+})
---
+++ X_OtherScripts/README.md
@@ -0,0 +1,2 @@
+# Other Scripts
+This folder contains random other Roll20 API scripts that folks might find helpful, generally created in response to requests on the forum.
